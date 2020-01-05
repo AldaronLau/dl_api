913aebee285cbdbecdc6b5c45b2d84788027c606
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-01-05 16:04:45 -0600

    Add enums.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::path::Path;
 use muon_rs as muon;
 use serde::{Deserialize, Serialize};
+use heck::CamelCase;
 
 #[derive(Serialize, Deserialize)]
 struct UnionVariant {
@@ -86,6 +87,47 @@ fn fail() -> String {
     std::process::exit(1);
 }
 
+fn convert(spec: &SafeFFI, mut out: String) -> String {
+    use std::fmt::Write;
+
+    // FIXME: Unions
+
+    for en in &spec.r#enum {
+        if let Some(ref doc) = en.doc {
+            out.push_str("/// ");
+            out.push_str(&doc.replace("\n", "\n/// "));
+            out.push_str("\n");
+        }
+        if let Some(ref ty) = en.r#type {
+            out.push_str("#[repr(");
+            out.push_str(ty);
+            out.push_str(")]\n");
+        }
+        out.push_str("#[repr(C)]\npub(crate) enum ");
+        out.push_str(&en.name);
+        out.push_str(" {\n");
+
+        for variant in &en.variant {
+            if let Some(ref doc) = variant.doc {
+                out.push_str("    /// ");
+                out.push_str(&doc.replace("\n", "\n    /// "));
+                out.push_str("\n");
+            }
+            out.push_str("    ");
+            out.push_str(&variant.name.to_camel_case());
+            if let Some(ref value) = variant.value {
+                out.push_str(" = ");
+                write!(out, "{}", value).unwrap();
+            }
+            out.push_str(",\n");
+        }
+
+        out.push_str("}\n\n");
+    }
+
+    out
+}
+
 fn main() {
     let mut args = std::env::args().skip(1);
 
@@ -100,7 +142,9 @@ fn main() {
         std::process::exit(2);
     });
 
-    let mut out = include_str!("../res/header.rs").to_string();
+    println!("Converting SafeFFI file: {} into a Rust file: {}", src, dst);
+
+    let out = convert(&cx, include_str!("../res/header.rs").to_string());
 
     let dst = Path::new(&dst);
 
@@ -113,8 +157,6 @@ fn main() {
 
     std::fs::write(dst, out).unwrap_or_else(|e| {
         eprintln!("Couldn't save file: '{}': {}!", dst.display(), e);
-        std::process::exit(3);
+        std::process::exit(4);
     });
-
-    println!("Converting SafeFFI file: {} into a Rust file: {}", src, dst.display());
 }
