2cbbf83387b9c47f24f39459c972c093c5272c34
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-01-12 19:35:52 -0600

    Fix more header bugs.

--- res/header.rs
+++ res/header.rs
@@ -22,13 +22,13 @@ unsafe fn new(name: &[u8]) -> Option<std::ptr::NonNull<std::ffi::c_void>> {
 }
 
 unsafe fn old(dll: std::ptr::NonNull<std::ffi::c_void>) {
-    assert_eq!(dlclose(dll), 0);
+    assert_eq!(dlclose(dll.as_ptr()), 0);
 }
 
 unsafe fn sym(dll: &std::ptr::NonNull<std::ffi::c_void>, name: &[u8])
     -> Option<std::ptr::NonNull<std::ffi::c_void>>
 {
-    std::ptr::NonNull::new(dlsym(dll, name.as_ptr().cast()))
+    std::ptr::NonNull::new(dlsym(dll.as_ptr(), name.as_ptr().cast()))
 }
 
 static mut THREAD_ID: std::mem::MaybeUninit<std::thread::ThreadId>
@@ -36,15 +36,24 @@ static mut THREAD_ID: std::mem::MaybeUninit<std::thread::ThreadId>
 static mut DLL: std::mem::MaybeUninit<std::ptr::NonNull<std::ffi::c_void>>
     = std::mem::MaybeUninit::uninit();
 static mut START_FFI: std::sync::Once = std::sync::Once::new();
+static mut SUCCESS: bool = false;
 
 unsafe fn check_thread() -> Option<std::ptr::NonNull<std::ffi::c_void>> {
+    assert_eq!(THREAD_ID.assume_init(), std::thread::current().id());
+
     START_FFI.call_once(|| {
         THREAD_ID = std::mem::MaybeUninit::new(std::thread::current().id());
         if let Some(dll) = new(DL_API_SHARED_OBJECT_NAME) {
-            DLL = dll;
+            DLL = std::mem::MaybeUninit::new(dll);
+        } else {
+            SUCCESS = true;
         }
     });
 
-    assert_eq!(THREAD_ID.assume_init(), std::thread::current().id());
+    if SUCCESS {
+        Some(DLL.assume_init())
+    } else {
+        None
+    }
 }
 
